2638693ddf2f8f1339c8bfec601c799739b8d910
Yuri <davydiuk.yurii@gmail.com>
2026-05-03 22:01:59 +0300

    fix

--- src/app/models/response.models.ts
+++ src/app/models/response.models.ts
@@ -7,3 +7,14 @@ export interface ResponseArray<T> {
   data: T[];
   error?: string;
 }
+export interface UserLoginResponse {
+  token: string;
+  userId: number;
+  email: string;
+}
+
+export interface UserRegisterResponse {
+  userId: number;
+  email: string;
+  createdAt: string; // ISO дата
+}
